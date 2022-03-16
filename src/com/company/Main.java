d1ec1fee682f7488ca552cca82784da4ca9bd968
edsmort <esmartqq@gmail.com>
2022-03-16 12:00:15 +1000

    took a step back. created one file implementation as base

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,13 @@
 package com.company;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
-    public static NameList generateNameList(File input) throws FileNotFoundException {
-        NameList names = new NameList();
+    public static List<String> generateNameList(File input) throws FileNotFoundException {
+        List<String> names = new ArrayList<String>();
         String name;
         BufferedReader reader = new BufferedReader(new FileReader(input));
         try {
@@ -18,12 +21,26 @@ public class Main {
         return names;
     }
 
+    public static void writeToFile(String fileName, List<String> nameList) {
+        String newFilename = fileName.replaceAll(".txt","-sorted.txt");
+        try {
+            FileWriter writer = new FileWriter(newFilename);
+            for (String name: nameList) {
+                writer.write(name + System.lineSeparator());
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         if (args.length == 1) { //TODO: allow multiple file path arguments
             File input = new File(args[0]);
             try {
-                NameList unsorted = generateNameList(input);
-                System.out.println(unsorted.toString());
+                List<String> nameList = generateNameList(input);
+                Collections.sort(nameList);
+                writeToFile(input.getName(), nameList);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
