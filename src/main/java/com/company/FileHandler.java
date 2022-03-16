bd479db0a3877143560ccccebc54a73df6c61108
edsmort <esmartqq@gmail.com>
2022-03-16 18:34:42 +1000

    Refactored code slightly to make testing easier

--- src/main/java/com/company/FileHandler.java
+++ src/main/java/com/company/FileHandler.java
@@ -5,31 +5,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileHandler {
-    File inputFile;
-    List<String> nameList;
 
-    public FileHandler(File inputFile) {
-        this.inputFile = inputFile ;
-        this.nameList = new ArrayList<String>();
-    }
-
-    public void readNamesFromFile() throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(this.inputFile));
-        reader.lines().forEach(name -> this.nameList.add(name));
+    public List<String> readNamesFromFile(File input) throws IOException {
+        List<String> nameList = new ArrayList<String>();
+        BufferedReader reader = new BufferedReader(new FileReader(input));
+        reader.lines().forEach(nameList::add);
         reader.close();
+        return nameList;
     }
 
-    public void writeOutputFile() throws IOException {
-        String outputFilename = inputFile.getName().replaceAll(".txt","-sorted.txt");
+    public void writeOutputFile(String outputFilename, List<String> nameList) throws IOException {
         FileWriter writer = new FileWriter(outputFilename);
-        for (String name: this.nameList) {
+        for (String name: nameList) {
             writer.write(name + System.lineSeparator());
         }
         writer.close();
     }
-
-    public List<String> getNameList() {
-        return this.nameList;
-    }
-
 }
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,23 +1,27 @@
 package com.company;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class Main {
+    static List<String> nameList;
 
     public static void main(String[] args) throws FileNotFoundException {
         if (args.length == 1) { //TODO: possible extension to allow multiple file path arguments
             File input = new File(args[0]);
-            FileHandler fileHandler = new FileHandler(input);
+            FileHandler fileHandler = new FileHandler();
             try {
-                fileHandler.readNamesFromFile();
+                nameList = fileHandler.readNamesFromFile(input);
             } catch (IOException e) {
                 System.err.println("Unable to read file, check that the file path you have provided is correct");
                 e.printStackTrace();
             }
-            Collections.sort(fileHandler.getNameList(), String.CASE_INSENSITIVE_ORDER);
+            nameList.sort(String.CASE_INSENSITIVE_ORDER);
             try {
-                fileHandler.writeOutputFile();
+                String outputFilename = input.getName().replaceAll(".txt","-sorted.txt");
+                fileHandler.writeOutputFile(outputFilename, nameList);
             } catch (IOException e) {
                 System.err.println("Error occurred while trying to write new file");
                 e.printStackTrace();
